f8fd9ee622ca90c93bc079ea35bdadf63f5596c9
Samarth Pillai <pillaisamarth@gmail.com>
2026-04-21 01:18:39 +0530

    Fxirst commit

---
+++ src/main/java/com/sp/sko/SkoApplication.java
@@ -0,0 +1,13 @@
+package com.sp.sko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkoApplication.class, args);
+	}
+
+}
