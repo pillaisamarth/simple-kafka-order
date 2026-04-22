e81637406ceae931cb1e9e9af13e4a20f14795f5
Samarth Pillai <pillaisamarth@gmail.com>
2026-04-23 02:05:23 +0530

    Send order-placed events to kafka topic

---
+++ src/main/java/com/sp/sko/config/KafkaConfig.java
@@ -0,0 +1,18 @@
+package com.sp.sko.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public NewTopic topic(){
+        return TopicBuilder.name("order-topic")
+                .partitions(2)
+                .replicas(1)
+                .build();
+    }
+}
--- src/main/java/com/sp/sko/controller/OrderController.java
+++ src/main/java/com/sp/sko/controller/OrderController.java
@@ -2,9 +2,14 @@ package com.sp.sko.controller;
 
 import com.sp.sko.model.OrderRequest;
 import com.sp.sko.model.TokenBucket;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.NonNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,14 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 @RequestMapping("/orders")
+@RequiredArgsConstructor
+@Slf4j
 public class OrderController {
     private static final int DEFAULT_BUCKET_CAPACITY = 5;
     private static final long DEFAULT_TOKEN_REFILL_PERIOD_MILLIS = 5000;
     private static final long DEFAULT_NUM_TOKENS_REFILL = 2;
+    private final KafkaTemplate<String, String> kafkaTemplate;
     private final Map<String, TokenBucket> buckets = new ConcurrentHashMap<>();
 
     @PostMapping
@@ -31,6 +40,16 @@ public class OrderController {
             System.out.println("[" + Thread.currentThread().getName() + "] - No tokens available");
             return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body("Insufficient tokens, try later\n");
         }
-        return ResponseEntity.ok("Order received with id: " + UUID.randomUUID() + "\n");
+        String uuid = UUID.randomUUID().toString();
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send("order-topic", uuid, "OrderPlaced for %s".formatted(request.userId()));
+        future.whenComplete((sr, ex) -> {
+            if(ex != null){
+                log.error("Error occured while publishing event to order topic", ex);
+            }else{
+                log.info("OrderPlaced event sent to order topic with uuid: {}, offset:{}, partition:{}", uuid, sr.getRecordMetadata().offset(), sr.getRecordMetadata().partition());
+            }
+        });
+        log.info("Sending response for id: {}", uuid);
+        return ResponseEntity.ok("Order received with id: " + uuid + "\n");
     }
 }
